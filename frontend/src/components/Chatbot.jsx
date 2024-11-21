503daac27bd4eecd4bf191c5b77c75d9c081864d
Turki <85177058+Turki25@users.noreply.github.com>
2024-11-21 11:57:51 +0000

    v1.0.3

--- frontend/src/components/Chatbot.jsx
+++ frontend/src/components/Chatbot.jsx
@@ -1,193 +1,3 @@
-// import { useState } from 'react';
-// import { useImmer } from 'use-immer';
-// import api from '@/api';
-// import { parseSSEStream } from '@/utils';
-// import ChatMessages from '@/components/ChatMessages';
-// import ChatInput from '@/components/ChatInput';
-
-// function Chatbot() {
-//   const [chatId, setChatId] = useState(null);
-//   const [messages, setMessages] = useImmer([]);
-//   const [newMessage, setNewMessage] = useState('');
-
-//   const isLoading = messages.length && messages[messages.length - 1].loading;
-
-//   async function submitNewMessage() {
-//     const trimmedMessage = newMessage.trim();
-//     if (!trimmedMessage || isLoading) return;
-
-//     setMessages(draft => [...draft,
-//     { role: 'user', content: trimmedMessage },
-//     { role: 'assistant', content: '', sources: [], loading: true }
-//     ]);
-//     setNewMessage('');
-
-//     let chatIdOrNew = chatId;
-//     try {
-//       if (!chatId) {
-//         const { id } = await api.createChat();
-//         setChatId(id);
-//         chatIdOrNew = id;
-//       }
-
-//       const stream = await api.sendChatMessage(chatIdOrNew, trimmedMessage);
-//       for await (const textChunk of parseSSEStream(stream)) {
-//         setMessages(draft => {
-//           draft[draft.length - 1].content += textChunk;
-//         });
-//       }
-//       setMessages(draft => {
-//         draft[draft.length - 1].loading = false;
-//       });
-//     } catch (err) {
-//       console.log(err);
-//       setMessages(draft => {
-//         draft[draft.length - 1].loading = false;
-//         draft[draft.length - 1].error = true;
-//       });
-//     }
-//   }
-
-//   return (
-//     <div className='relative grow flex flex-col gap-6 pt-6'>
-//       {messages.length === 0 && (
-//         <div className='mt-3 font-urbanist text-primary-blue text-xl font-light space-y-2'>
-//           <p>👋 Welcome!</p>
-//           <p>I’m your AI Analysis Advisor, here to help with stock forecasts, cryptocurrency trends, and company risk analysis. I provide clear, actionable insights for smarter financial decisions.</p>
-//           <p>Ask me!</p>
-//         </div>,
-//         <div
-//           className='mt-3 font-urbanist text-primary-blue text-xl font-light space-y-2'
-//           dir='rtl'
-//           style={{ textAlign: 'right' }}
-//         >
-//           <p>👋 اهلاً وسهلاً!</p>
-//           <p>أنا مستشارك الذكي لتحليل الأسواق المالية، أقدم توقعات الأسهم، وتحليلات العملات الرقمية، وتقييم مخاطر الشركات. أوفر لك رؤى واضحة وقرارات مالية أكثر ذكاءً.</p>
-//           <p>اسألني!</p>
-//         </div>
-
-//       )}
-//       <ChatMessages
-//         messages={messages}
-//         isLoading={isLoading}
-//       />
-//       <ChatInput
-//         newMessage={newMessage}
-//         isLoading={isLoading}
-//         setNewMessage={setNewMessage}
-//         submitNewMessage={submitNewMessage}
-//       />
-//     </div>
-//   );
-// }
-// export default Chatbot;
-// import { useState } from 'react';
-// import { useImmer } from 'use-immer';
-// import api from '@/api';
-// import ChatMessages from '@/components/ChatMessages';
-// import ChatInput from '@/components/ChatInput';
-// import Base64ImageViewer from '@/components/Base64ImageViewer';
-
-
-// function Chatbot() {
-//   const [messages, setMessages] = useImmer([]);
-//   const [newMessage, setNewMessage] = useState('');
-
-//   const isLoading = messages.length && messages[messages.length - 1].loading;
-
-//   async function submitNewMessage() {
-//     const trimmedMessage = newMessage.trim();
-//     if (!trimmedMessage || isLoading) return;
-  
-//     // Add user's message to the chat history
-//     setMessages(draft => [
-//       ...draft,
-//       { role: 'user', content: trimmedMessage },
-//       { role: 'assistant', content: '', loading: true }
-//     ]);
-//     setNewMessage('');
-  
-//     try {
-  
-//       // Construct the chat history
-//       const chatHistory = messages
-//         .filter(message => message.role === 'user' || message.role === 'assistant')
-//         .map(message => ({ role: message.role, content: message.content }));
-  
-//       // Create the payload
-//       const payload = {
-//         chat_input: trimmedMessage,
-//         chat_history: [] ,//chatHistory,
-//       };
-  
-//       // Send request to the API
-//       const response = await api.sendChatMessage(payload);
-//       // console.log(response); // Verify structure
-//       // const { reply, sources } = response;
-
-
-//       const  reply   = response["answer"];
-//       const  arima_forecast   = response["arima_forecast"];
-//       const  candlestick_charts   = response["candlestick_charts"];
-//       const  ma_20   = response["ma_20"];
-
-
-//       const sources = [];
-
-//       // Update the assistant's message with the reply
-//       setMessages(draft => {
-//         draft[draft.length - 1].content = reply;
-//         draft[draft.length - 1].loading = false;
-//         if (sources) draft[draft.length - 1].sources = sources;
-//       });
-//     } catch (err) {
-//       console.error('Error during API call:', err);
-//       setMessages(draft => {
-//         draft[draft.length - 1].loading = false;
-//         draft[draft.length - 1].error = true;
-//       });
-//     }
-//   }
-  
-
-//   return (
-//     <div className='relative grow flex flex-col gap-6 pt-6'>
-//       {messages.length === 0 && (
-//         <div className='mt-3 font-urbanist text-primary-blue text-xl font-light space-y-2'>
-//           <p>👋 Welcome!</p>
-//           <p>I’m your AI Analysis Advisor, here to help with stock forecasts, cryptocurrency trends, and company risk analysis. I provide clear, actionable insights for smarter financial decisions.</p>
-//           <p>Ask me!</p>
-//         </div>,
-//         <div
-//           className='mt-3 font-urbanist text-primary-blue text-xl font-light space-y-2'
-//           dir='rtl'
-//           style={{ textAlign: 'right' }}
-//         >
-//           <p>👋 اهلاً وسهلاً!</p>
-//           <p>أنا مستشارك الذكي لتحليل الأسواق المالية، أقدم توقعات الأسهم، وتحليلات العملات الرقمية، وتقييم مخاطر الشركات. أوفر لك رؤى واضحة وقرارات مالية أكثر ذكاءً.</p>
-//           <p>اسألني!</p>
-//         </div>
-//       )}
-//       <ChatMessages
-//         messages={messages}
-//         isLoading={isLoading}
-//       />
-//       <Base64ImageViewer base64String={arima_forecast} />
-//       <Base64ImageViewer base64String={candlestick_charts} />
-//       <Base64ImageViewer base64String={ma_20} />
-
-//       <ChatInput
-//         newMessage={newMessage}
-//         isLoading={isLoading}
-//         setNewMessage={setNewMessage}
-//         submitNewMessage={submitNewMessage}
-//       />
-//     </div>
-//   );
-// }
-
-// export default Chatbot;
-//----------
 import { useState } from 'react';
 import { useImmer } from 'use-immer';
 import api from '@/api';
@@ -214,15 +24,23 @@ function Chatbot() {
     setNewMessage('');
 
     try {
-      // Construct the chat history
-      const chatHistory = messages
-        .filter(msg => msg.role === 'user' || msg.role === 'assistant')
-        .map(msg => ({ role: msg.role, content: msg.content }));
+      // Construct chat history for the payload
+      const formattedHistory = messages.map((msg, index) => ({
+        inputs: {
+          chat_input: msg.role === 'user' ? msg.content : null,
+        },
+        outputs: {
+          answer: msg.role === 'assistant' ? msg.content : null,
+          arima_forecast: msg.base64Images?.[0] || null,
+          candlestick_charts: msg.base64Images?.[1] || null,
+          ma_20: msg.base64Images?.[2] || null,
+        },
+      }));
 
       // Create the payload
       const payload = {
         chat_input: trimmedMessage,
-        chat_history: [],
+        chat_history: formattedHistory,
       };
 
       // Send request to the API
@@ -282,16 +100,18 @@ function Chatbot() {
       )}
 
       {/* Chat Messages */}
-      <ChatMessages messages={messages} isLoading={isLoading} />
-
-      {/* Render Base64 Images for the Latest Assistant Message */}
-      {messages.length > 0 && messages[messages.length - 1].base64Images?.length > 0 && (
-        <div className="flex flex-wrap gap-4">
-          {messages[messages.length - 1].base64Images.map((image, index) => (
-            <Base64ImageViewer key={index} base64String={image} />
-          ))}
+      {messages.map((message, index) => (
+        <div key={index} className="flex flex-col gap-4">
+          <ChatMessages messages={[message]} />
+          {message.base64Images?.length > 0 && (
+            <div className="flex flex-wrap gap-4">
+              {message.base64Images.map((image, imgIndex) => (
+                <Base64ImageViewer key={imgIndex} base64String={image} />
+              ))}
+            </div>
+          )}
         </div>
-      )}
+      ))}
 
       {/* Chat Input */}
       <ChatInput
